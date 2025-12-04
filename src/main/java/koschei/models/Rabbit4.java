fb1ce48613df5395406ee0f76ac90579ce34d3f9
Nikita <mrcoopgmz@gmail.com>
2025-12-04 22:20:21 +0300

    Третье задание выполнено

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,16 @@
 package koschei.models;
 
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
 public class Rabbit4 {
 
+    @Autowired
+    private Duck5 duck;
+
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck.toString();
     }
 }
